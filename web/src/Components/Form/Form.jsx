01f3105e9c9ec777e57dc2ac78f22f16a3ce4c7c
dohatir <dohagit0@gmail.com>
2024-05-19 22:10:09 +0100

    link1

--- web/src/Components/Form/Form.jsx
+++ web/src/Components/Form/Form.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 
 const Form = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission logic here
-  };
 
+  const [titre, setTitre]= useState("");
+  const [description, setDescription]= useState("");
+  const [commite, setCommite]= useState("");
+  const [types, setType]= useState("");
+  const [start_date, setStartdate]= useState("");
+  const [end_date, setEnddate]= useState("");
+  const [besoin, setBesoin]= useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    formData.set("titre",titre);
+    formData.set("description",description);
+    formData.set("commite",commite);
+    formData.set("types",types);
+    formData.set("start_date",start_date);
+    formData.set("end_date",end_date);
+    formData.set("besoin",besoin);
+
+    try {
+      const response = await fetch('http://127.0.0.1:8000/dem/org/add/', {
+        method: 'POST',
+        body: formData
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        // Traitez la réponse du backend ici
+      } else {
+        throw new Error('Request failed');
+      }
+    } catch (error) {
+      console.error(error);
+      // Gérez les erreurs ici
+    }
+  };
+  
   return (
     <div className=" flex items-center justify-center m-8">
       <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray-100">
@@ -30,6 +64,8 @@ const Form = () => {
               type="text"
               id="event_title"
               name="event_title"
+              value={titre}
+              onChange={(e)=> setTitre(e.target.value)}
               className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
               placeholder="Enter Event Title"
               required
@@ -43,6 +79,8 @@ const Form = () => {
             <textarea
               id="event_description"
               name="event_description"
+              value={description}
+              onChange={(e)=> setDescription(e.target.value)}
               className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
               placeholder="Enter Event Description"
               rows="4"
@@ -61,6 +99,8 @@ const Form = () => {
               type="text"
               id="organizing_committee"
               name="organizing_committee"
+              value={commite}
+              onChange={(e)=> setCommite(e.target.value)}
               className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
               placeholder="Enter Organizing Committee"
               required
@@ -75,6 +115,8 @@ const Form = () => {
               type="text"
               id="event_type"
               name="event_type"
+              value={types}
+              onChange={(e)=> setType(e.target.value)}
               className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
               placeholder="Enter Event Type"
               required
@@ -90,6 +132,8 @@ const Form = () => {
                 type="date"
                 id="start_date"
                 name="start_date"
+                value={start_date}
+                onChange={(e)=> setStartdate(e.target.value)}
                 className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
                 required
               />
@@ -102,6 +146,8 @@ const Form = () => {
                 type="date"
                 id="end_date"
                 name="end_date"
+                value={end_date}
+                onChange={(e)=> setEnddate(e.target.value)}
                 className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
                 required
               />
@@ -115,6 +161,8 @@ const Form = () => {
             <textarea
               id="requirements"
               name="requirements"
+              value={besoin}
+              onChange={(e)=> setBesoin(e.target.value)}
               className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
               placeholder="Enter Event Requirements (e.g., Venue, Financial Support)"
               rows="4"
