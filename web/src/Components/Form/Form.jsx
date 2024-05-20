e070acc3095fe61e05792f2758f5fd042645c433
chaimaert <chaima.Rouita@emsi-edu.ma>
2024-05-20 11:53:48 +0100

    Update form.jsx

--- web/src/Components/Form/Form.jsx
+++ web/src/Components/Form/Form.jsx
@@ -2,32 +2,34 @@ import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 
 const Form = () => {
+  const [titre, setTitre] = useState("");
+  const [description, setDescription] = useState("");
+  const [commite, setCommite] = useState("");
+  const [types, setType] = useState("");
+  const [start_date, setStartdate] = useState("");
+  const [end_date, setEnddate] = useState("");
+  const [besoin, setBesoin] = useState("");
 
-  const [titre, setTitre]= useState("");
-  const [description, setDescription]= useState("");
-  const [commite, setCommite]= useState("");
-  const [types, setType]= useState("");
-  const [start_date, setStartdate]= useState("");
-  const [end_date, setEnddate]= useState("");
-  const [besoin, setBesoin]= useState("");
+  // Obtenir la date actuelle
+  const today = new Date().toISOString().split("T")[0];
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    formData.set("titre",titre);
-    formData.set("description",description);
-    formData.set("commite",commite);
-    formData.set("types",types);
-    formData.set("start_date",start_date);
-    formData.set("end_date",end_date);
-    formData.set("besoin",besoin);
+    formData.set("titre", titre);
+    formData.set("description", description);
+    formData.set("commite", commite);
+    formData.set("types", types);
+    formData.set("start_date", start_date);
+    formData.set("end_date", end_date);
+    formData.set("besoin", besoin);
 
     try {
       const response = await fetch('http://127.0.0.1:8000/dem/org/add/', {
         method: 'POST',
         body: formData
       });
-  
+
       if (response.ok) {
         const data = await response.json();
         console.log(data);
@@ -40,9 +42,9 @@ const Form = () => {
       // Gérez les erreurs ici
     }
   };
-  
+
   return (
-    <div className=" flex items-center justify-center m-8">
+    <div className="flex items-center justify-center m-8">
       <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray-100">
         <div className="flex items-center mb-6">
           <img src={logo} alt="EventHub Logo" className="w-12 h-12 mr-4" />
@@ -65,7 +67,7 @@ const Form = () => {
               id="event_title"
               name="event_title"
               value={titre}
-              onChange={(e)=> setTitre(e.target.value)}
+              onChange={(e) => setTitre(e.target.value)}
               className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
               placeholder="Enter Event Title"
               required
@@ -80,7 +82,7 @@ const Form = () => {
               id="event_description"
               name="event_description"
               value={description}
-              onChange={(e)=> setDescription(e.target.value)}
+              onChange={(e) => setDescription(e.target.value)}
               className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
               placeholder="Enter Event Description"
               rows="4"
@@ -89,10 +91,7 @@ const Form = () => {
           </div>
 
           <div className="mt-6">
-            <label
-              className="text-lg font-medium"
-              htmlFor="organizing_committee"
-            >
+            <label className="text-lg font-medium" htmlFor="organizing_committee">
               Organizing Committee
             </label>
             <input
@@ -100,7 +99,7 @@ const Form = () => {
               id="organizing_committee"
               name="organizing_committee"
               value={commite}
-              onChange={(e)=> setCommite(e.target.value)}
+              onChange={(e) => setCommite(e.target.value)}
               className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
               placeholder="Enter Organizing Committee"
               required
@@ -116,7 +115,7 @@ const Form = () => {
               id="event_type"
               name="event_type"
               value={types}
-              onChange={(e)=> setType(e.target.value)}
+              onChange={(e) => setType(e.target.value)}
               className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
               placeholder="Enter Event Type"
               required
@@ -133,7 +132,8 @@ const Form = () => {
                 id="start_date"
                 name="start_date"
                 value={start_date}
-                onChange={(e)=> setStartdate(e.target.value)}
+                onChange={(e) => setStartdate(e.target.value)}
+                min={today} 
                 className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
                 required
               />
@@ -147,7 +147,8 @@ const Form = () => {
                 id="end_date"
                 name="end_date"
                 value={end_date}
-                onChange={(e)=> setEnddate(e.target.value)}
+                onChange={(e) => setEnddate(e.target.value)}
+                min={today} 
                 className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
                 required
               />
@@ -162,7 +163,7 @@ const Form = () => {
               id="requirements"
               name="requirements"
               value={besoin}
-              onChange={(e)=> setBesoin(e.target.value)}
+              onChange={(e) => setBesoin(e.target.value)}
               className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1"
               placeholder="Enter Event Requirements (e.g., Venue, Financial Support)"
               rows="4"
